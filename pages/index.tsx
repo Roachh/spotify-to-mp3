908b6e23dea696c10d64d8fd77db25a845a2148e
Roachh <thgebarata@gmail.com>
2022-07-07 22:59:07 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,95 @@
+import { Button, Container, Grid, TextField } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useEffect, useState } from "react";
+
+const CGrid = styled(Grid)`
+  padding-top: 150px;
+  justify-content: center;
+`;
+
+const CButton = styled(Button)`
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+`;
+
+const CTextField = styled(TextField)`
+  & .MuiOutlinedInput-root {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+`;
+
+export default function Home({ token }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(event.target.value);
+  };
+
+  useEffect(() => {
+    setAccessToken(token);
+    console.log(`token: ${token}`);
+  }, []);
+
+  async function search() {
+    console.log("Search for " + searchInput);
+
+    let playlistParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    fetch(
+      `https://api.spotify.com/v1/playlists/${searchInput}/tracks`,
+      playlistParameters
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data.items));
+  }
+
+  return (
+    <Container>
+      <CGrid container>
+        <CTextField
+          label="Playlist ID"
+          variant="outlined"
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              search();
+            }
+          }}
+          value={searchInput}
+          onChange={handleChange}
+        />
+        <CButton variant="contained" onClick={search}>
+          Search
+        </CButton>
+      </CGrid>
+
+      <Grid container></Grid>
+    </Container>
+  );
+}
+
+export async function getServerSideProps() {
+  var authParameters = {
+    method: "POST",
+    headers: {
+      "Content-type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=client_credentials&client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`,
+  };
+
+  let result = await fetch(
+    "https://accounts.spotify.com/api/token",
+    authParameters
+  );
+  let data = await result.json();
+  let token = await data.access_token;
+
+  return { props: { token } };
+}
